2249fb493b77c83bab66a25a543833f494c2c901
章礼平 <LipperZack@gmail.com>
2015-08-01 09:34:41 +0800

    add guid

--- src/piczoom.js
+++ src/piczoom.js
@@ -880,6 +880,17 @@ $(function(){
 });
 
 $(function () {
+
+    //https://github.com/vczero/OurTimes/blob/master/server/util/guid.js
+    window.Guid =function(){
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        }).toUpperCase();
+    };
+
+
     $('div.pic-zoom').each(function () {
         new PicZoom($(this), {});
     });
