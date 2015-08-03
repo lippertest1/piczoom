5041b388bf014f59b602623e535f975dc127fafd
章礼平 <LipperZack@gmail.com>
2015-08-03 22:06:43 +0800

    update grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,27 @@ module.exports = function (grunt) {
   // 项目配置
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {  
+      options: {  
+      },  
+      dist: {  
+        src: ['src/zepto.min.js','src/plupload.full.min.js','src/Slide.js','src/zoom.js','src/canvas.js','src/qiniu.js','src/piczoom.js'],//src文件夹下包括子文件夹下的所有文件  
+        dest: 'dest/built.js'//合并文件在dist下名为built.js的文件  
+      }  
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%=pkg.file %>.js',
+        src: 'dest/built.js',
         dest: 'dest/<%= pkg.file %>.min.js'
       }
     }
   });
   // 加载提供"uglify"任务的插件
+  grunt.loadNpmTasks('grunt-contrib-concat');  
   grunt.loadNpmTasks('grunt-contrib-uglify');
   // 默认任务
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat','uglify']);  
 }
